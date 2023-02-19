5e1191d52beeb94f11a6c33410bf05bf9e923105
visavesj-dev <desper011@hotmail.co.th>
2023-02-19 13:25:55 +0700

    chore:

--- src/ArticleContent.js
+++ src/ArticleContent.js
@@ -3,10 +3,6 @@ const articles = [
     name: "ชุดที่ 1",
     img: "nongnai.jpg",
   },
-  {
-    name: "ชุดที่ 2",
-    img: "hood.png",
-  },
 ];
 
 export default articles;
--- src/List.js
+++ src/List.js
@@ -2,8 +2,18 @@ import React from "react";
 import ArticleContent from "./ArticleContent";
 import ArticleList from "./ArticleList";
 const List = () => (
-  <div>
+  <div
+    style={{
+      display: "flex",
+      flexDirection: "column",
+      justifyContent: "center",
+      alignItems: "center",
+    }}
+  >
     <img src="landing.png" alt="landing" width={1000} />
+    <video width="320" height="240" controls>
+      <source src="nongnai.mp4" type="video/mp4" />
+    </video>
     <ArticleList articles={ArticleContent} />
   </div>
 );
