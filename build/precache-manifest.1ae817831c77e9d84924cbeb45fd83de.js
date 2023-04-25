3ff8a4654dfccb6e57bbf73d04fb1d6eae6adeba
root <desper011@hotmail.co.th>
2023-04-25 03:42:14 +0000

    hello

---
+++ build/precache-manifest.1ae817831c77e9d84924cbeb45fd83de.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "584035ad319e4a6fad82",
+    "url": "/static/css/main.8f2df384.chunk.css"
+  },
+  {
+    "revision": "584035ad319e4a6fad82",
+    "url": "/static/js/main.38fe41de.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "e2d865ec66fb28eff10b",
+    "url": "/static/css/2.f5f952fb.chunk.css"
+  },
+  {
+    "revision": "e2d865ec66fb28eff10b",
+    "url": "/static/js/2.10e97056.chunk.js"
+  },
+  {
+    "revision": "649af03226b2717624c43833e1c68105",
+    "url": "/index.html"
+  }
+];
