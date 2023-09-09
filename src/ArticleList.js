70add543aadb0109b47ed47d4a183ed7745a8a11
Visavesj <desper011@hotmail.co.th>
2023-09-09 22:33:33 +0700

    init

--- src/ArticleList.js
+++ src/ArticleList.js
@@ -1,22 +1,33 @@
 import React from "react";
 import { Helmet } from "react-helmet";
 import { Link } from "react-router-dom";
+import { FacebookProvider, Page, EmbeddedPost } from "react-facebook";
+
 const ArticleList = ({ articles }) => (
   <div style={{ marginTop: "20px", marginBottom: "100px" }}>
     <Helmet>
       <meta content="index, follow" name="robots" />
     </Helmet>
-    {articles.map((article, key) => (
-      <Link
-        className="btn btn-outline-danger rounded border border-white p-3 col-6 col-md-4 uk-animation-slide-top"
-        key={key}
-        to={`/article/${article.name}`}
-      >
-        <img className="img-fluid rounded" src={article.img} />
+    <div>
+      <FacebookProvider appId="318515543995560">
+        <Page href="https://www.facebook.com/ruksanongnai" tabs="timeline">
+       asfdasfdasdfasfsaf
+        </Page>
+      </FacebookProvider>
+    </div>
+    <div>
+      {articles.map((article, key) => (
+        <Link
+          className="btn btn-outline-danger rounded border border-white p-3 col-6 col-md-4 uk-animation-slide-top"
+          key={key}
+          to={`/article/${article.name}`}
+        >
+          <img className="img-fluid rounded" src={article.img} />
 
-        <div style={{ marginTop: "20px" }}>{article.title}</div>
-      </Link>
-    ))}
+          <div style={{ marginTop: "20px" }}>{article.title}</div>
+        </Link>
+      ))}
+    </div>
   </div>
 );
 
